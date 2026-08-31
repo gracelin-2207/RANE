0e5d6beda8a1be0561d79630202fd9ca73bfb238
GRACELIN <gracelin.27csa@licet.ac.in>
2026-08-31 16:15:17 +0530

    Fix CORS for local development

--- api/generate-qr.js
+++ api/generate-qr.js
@@ -1,6 +1,30 @@
 import qr from 'qr-image';
 
 export default function handler(req, res) {
+  const origin = req.headers.origin;
+
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://rmqr-rane-madras-limited.vercel.app"
+];
+
+if (allowedOrigins.includes(origin)) {
+  res.setHeader("Access-Control-Allow-Origin", origin);
+}
+
+res.setHeader(
+  "Access-Control-Allow-Methods",
+  "POST, OPTIONS"
+);
+
+res.setHeader(
+  "Access-Control-Allow-Headers",
+  "Content-Type"
+);
+
+if (req.method === "OPTIONS") {
+  return res.status(200).end();
+}
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Only POST requests allowed' });
   }
